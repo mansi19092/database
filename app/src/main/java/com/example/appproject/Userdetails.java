774793e88613aacda1bd50ffb9c836e08ea699a4
mansi19092 <mansi19092@gmail.com>
2019-10-22 17:15:09 +0530

    new file!!@mansi

---
+++ app/src/main/java/com/example/appproject/Userdetails.java
@@ -0,0 +1,14 @@
+package com.example.appproject;
+
+public class Userdetails {
+    String Username,email;
+
+    public Userdetails() {
+        //to read the values back
+    }
+
+    public Userdetails(String username, String email) {
+        Username = username;
+        this.email = email;
+    }
+}
